3bd34cc77eab2f26158db04a300e63e8f6015273
senadev42 <senadevv42@gmail.com>
2024-04-29 02:05:53 +0300

    added validation pipe to nest app

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,18 @@ import { AppModule } from './app.module';
 
 //Modules
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   //configs
   app.setGlobalPrefix('api/v1');
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
   const configService: ConfigService = app.get<ConfigService>(ConfigService);
   const port = configService.get('PORT') || 3000;
-  
+
   await app.listen(port);
 }
-bootstrap(); 
+bootstrap();
